31f2dd8fbb63e5babf3e36f4a8c40561aa14b798
Dinara <dinara198989@bk.ru>
2022-03-22 17:04:20 +0100

    Added Todolist component

--- trello/src/App.tsx
+++ trello/src/App.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {TaskType, Todolist} from "./components/Todolist";
 
 function App() {
+
+    let tasks1: Array<TaskType> = [
+        { id: 1, title: "CSS", isDone: true},
+        { id: 2, title: "HTML", isDone:  true},
+        { id: 3, title: "JS", isDone:  true},
+        { id: 4, title: "Redux", isDone: false},
+        { id: 5, title: "React", isDone: false}
+    ]
+
+    let tasks2: Array<TaskType> = [
+        { id: 1, title: "Terminator", isDone:  true},
+        { id: 2, title: "XXX", isDone:  true},
+        { id: 3, title: "Gentelmens", isDone:  true},
+        { id: 4, title: "Matrix", isDone: false},
+        { id: 5, title: "Love", isDone: false}
+    ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Todolist title={"Want to learn"} tasks={tasks1}/>
+      <Todolist title={"Movies"} tasks={tasks2}/>
     </div>
   );
 }
